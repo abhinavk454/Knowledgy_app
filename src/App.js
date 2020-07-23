abaf791e2268542b88f20bc822a15136208fc63e
Abhinav Kumar <abhinav.vikash2@gmail.com.com>
2020-07-24 01:33:02 +0530

    compleated v01

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React,{useState} from 'react'
-import News from './components/News';
+import React from 'react';
+import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
+import './App.css'
+import Page01 from './components/Page01';
+import Page02 from './components/Page02';
+import Page03 from './components/Page03';
+import Page04 from './components/Page04';
+import Page05 from './components/Page05';
 
 function App() {
-    const [ques,setQues]=useState("");
-    const [search,setSearch]=useState("");
+    // const [ques,setQues]=useState("");
+    // const [search,setSearch]=useState("");
     return (
-        <div>
-            <div>
+        <div className="all">
+            <div className="intro">
+            <h1>KNOWLEDGY</h1>
+            <article id="art">
+                This web app provides information and current<br/> useful knowledge about topics which we needs to understand,<br/>
+                the information of these topics will solve a lot of problems in human life. 
+            </article>
+            </div>
+        <div className="maina">
+            <Router>
+                <Link className="Link" to="/">Home</Link>
+                <Link className="Link" to="/page01">Humanitarianism</Link>
+                <Link className="Link" to="/page02">Sustanibility</Link>
+                <Link className="Link" to="/page03">Health</Link>
+                <Link className="Link" to="/page04">Education</Link>
+                <Link className="Link" to="/page05">Inequality</Link>
+                <Switch>
+                    <Route exact path="/"/>
+                    <Route path="/page01" component={Page01}/>
+                    <Route path="/page02" component={Page02}/>
+                    <Route path="/page03" component={Page03}/>
+                    <Route path="/page04" component={Page04}/>
+                    <Route path="/page05" component={Page05}/>
+                </Switch>
+            </Router>
+            {/* <div>
                 <form>
                     <input type="text" value={ques} onChange={(e)=>{setQues(e.target.value)}}/>
                     {/* <input type="submit" value="Submit" onClick={()=>{setSearch(ques)}}/> */}
-                </form>
+                {/* </form>
                 <p>{ques}</p>
-            </div>
+            </div> */}
+            {/* <div>
             <News topic={ques}/>
+            // </div> */}
+        </div>
         </div>
     )
 }
--- src/components/Page05.jsx
+++ src/components/Page05.jsx
@@ -1,36 +1,46 @@
-import React, { useEffect,useState } from 'react'
-import axios from 'axios'
-import "./News.css"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "./News.css";
 //inequality
 function Page05() {
-    const [news,setNews]=useState([]) 
-    useEffect(()=>{
-        axios.get(`http://newsapi.org/v2/everything?q=Inequality+Issue&sortBy=popularity&apiKey=498ea298d85448b0b4efb9d95ffb002f`)
-        .then((res)=>{
-            console.log(res.data.articles)
-            setNews(res.data.articles)
-        })
-        .catch((err)=>{console.error(err)})
-    },[topic])// eslint-disable-line
-    return (
-        <div className="main">
-            {news.map((re,index)=>(
-                <div className="inner">
-                    <article id="art" key={re.source.id}>
-                        <img id ="img" src={re.urlToImage} alt=""/>
-                        <br />
-                        <span id="sp1">Title : {re.title}</span>
-                        <br /> 
-                        Author : {re.author}
-                        <br />
-                        Content : {re.content}
-                        <br />
-                        <a href={re.url} target="_blank">For more</a>
-                    </article>
-                </div>
-                ))}
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `http://newsapi.org/v2/everything?q=Inequality&sortBy=popularity&apiKey=498ea298d85448b0b4efb9d95ffb002f`
+      )
+      .then((res) => {
+        setNews(res.data.articles);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []); // eslint-disable-line
+  return (
+    <div className="main">
+      {news.map((re, index) => (
+        <div className="inner">
+          <article id="art" key={re.source.id}>
+            <img id="img" src={re.urlToImage} alt="" />
+            <br />
+            <span id="sp1">Title : {re.title}</span>
+            <br />
+            Author : {re.author}
+            <br />
+            Content : {re.content}
+            <br />
+            <a href={re.url} target="_blank">
+              For more
+            </a>
+            <br />
+            <span>By </span>: {re.source.name}
+            <br />
+            <span>Date </span>: {re.publishedAt}
+          </article>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
 export default Page05;
